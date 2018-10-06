7d1a54dd3ab8e314a25c9998c2c09fb79f6c6c71
isneverdead <richart135@gmail.com>
2018-10-06 11:53:40 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -5,7 +5,7 @@
  $title=$_POST['title'];
  $duration=$_POST['duration'];
  $price=$_POST['price'];
- $q=mysqli_query($con,"INSERT INTO `course_details` (`title`,`duration`,`price`) VALUES ('$title','$duration','$price')");
+ $q=mysqli_query($con,"INSERT INTO `id` (`title`,`duration`,`price`) VALUES ('$title','$duration','$price')");
  if($q)
   echo "success";
  else
